14ab4020f42f6292d94fd62328d1d55ca5baf181
Keenan-04 <keenanmccall@outlook.com>
2025-08-28 11:36:58 -0500

    Implemented the extract and compile, should work maybe

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,10 +1,7 @@
-
 #!/bin/bash
-
-tar -xvzf NthPrime.tgz
-
-cd NthPrime 
-
-gcc -o NthPrime NthPrimce.c
-
+tar -xvzf ./NthPrime.tgz
+cd NthPrime || exit
+gcc -o NthPrime nth_prime.c main.c
 ./NthPrime
+
+
