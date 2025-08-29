495e451b1c313e94008c1c71a82542b7341d8941
Keenan-04 <keenanmccall@outlook.com>
2025-08-28 19:55:11 -0500

    added some more code for cleaning, trying out grep to read each line and rm, not sure yet

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 directoryused=$1
-name=$(name "$directoryused", .tgz)
-
+name=$(basename "$directoryused", .tgz)
+here=$(pwd)
+scratch=$(mktemp -d)
+tar -xzf "$directoryused" -c $scratch
+grep -rl "delete me" . | while read -r file; do
+	rm "$file"
+done
+cd $here
+tar -czf "cleaned_$directoryused":
 
 
