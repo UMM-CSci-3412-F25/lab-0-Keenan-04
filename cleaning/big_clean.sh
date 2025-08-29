42698a28e5e5310a79be545245a413f66e2d82e7
Keenan-04 <keenanmccall@outlook.com>
2025-08-28 20:10:04 -0500

    This should be the right format, just need to figure out if the cleaning is actually working

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -3,11 +3,9 @@ directoryused=$1
 name=$(basename "$directoryused", .tgz)
 here=$(pwd)
 scratch=$(mktemp -d)
-tar -xzf "$directoryused" -c $scratch
+tar -xzf "$directoryused" -c "$scratch"
 grep -rl "delete me" . | while read -r file; do
 	rm "$file"
 done
-cd $here
-tar -czf "cleaned_$directoryused":
-
-
+cd "$scratch" || exit
+tar -czf "$here/cleaned_$directoryused" "$name"
